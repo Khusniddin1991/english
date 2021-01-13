ee7d517b8b42b8070d6531d08c6ca013df16b682
Khusniddin1991 <husni1994tesla@gmail.com>
2021-01-13 02:19:16 -0800

    masseges

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class MyHomePage extends StatefulWidget {
+  static const String id='home_page';
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('value').tr(),centerTitle: true,),
+      body:SafeArea(
+        child:Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment:MainAxisAlignment.end,
+            children: [
+              Text("value",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w800),).tr(),
+                SizedBox(height: 300,),
+
+              Row(children: [
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    color: Colors.red,
+                    child: FlatButton(
+                      onPressed: (){
+                        setState(() {
+                          context.locale = Locale('en', 'US');
+                        });
+                      },
+                      child: Text('English',style: TextStyle(color: Colors.white,fontSize: 13))
+                    ),
+
+                  ),
+                ),SizedBox(width: 10,),
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    color: Colors.green,
+                    child: FlatButton(
+                        onPressed: (){
+                          setState(() {
+                            context.locale = Locale('ru', 'RU');
+                          });
+                        },
+                        child: Text('Russian',style: TextStyle(fontSize: 13,color: Colors.white))
+                    ),
+
+                  ),
+                ),SizedBox(width: 10,),Expanded(
+                  child: Container(
+                    height: 50,
+                    color: Colors.blue,
+                    child: FlatButton(
+                        onPressed: (){
+                          setState(() {
+                            context.locale = Locale('uz', 'UZ');
+                          });
+                        },
+                        child: Text('Uzbek',style: TextStyle(color: Colors.white,fontSize: 13),)
+                    ),
+
+                  ),
+                ),SizedBox(width: 10,),Expanded(
+                  child: Container(
+                    height: 50,
+                    color: Colors.indigo,
+                    child: FlatButton(
+                        onPressed: (){
+                          setState(() {
+                            context.locale = Locale('fr', 'FR');
+                          });
+                        },
+                        child: Text('Franch',style: TextStyle(color: Colors.white,fontSize: 13),)
+                    ),
+
+                  ),
+                ),
+              ],)
+              ,SizedBox(height: 20,)
+                ],),
+
+        )
+      ),
+    );
+  }
+}
